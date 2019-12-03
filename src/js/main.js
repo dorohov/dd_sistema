1d3becbfc9f6aa83ff4a367a2da302703d5ed23e
Pavel Proyavin <pavel@dorohovdesign.com>
2019-12-03 11:22:05 +0300

    release

--- src/js/main.js
+++ src/js/main.js
@@ -21,7 +21,29 @@
                         })
                 }
             })
+
+            var blocksTextBg = $('.__dd__bg-text')
+
+            blocksTextBg.each(function() {
+
+                var thisBlock = $(this)
+
+                if($(this).visible(true) && !$(this).hasClass('__dd__is--show')) {
+                    thisBlock
+                        .addClass('__dd__is--show')
+                }
+            })
         }
+
+        $('.anchor').on('click', function(e) {
+            e.preventDefault();
+            var _this = $(this)
+            var aid = _this.attr("href");
+            if(!aid) {
+                aid = _this.data('target')
+            }
+            $('html,body').animate({scrollTop: $(aid).offset().top},'slow');
+        })
 
         startAnimationBlocks()
 
